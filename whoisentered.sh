30de3395ae58618cbacadb7867e86f7b709e6ab5
godlakes <gamessuperfunny@gmail.com>
2026-05-14 20:13:25 +0500

    fixed

--- whoisentered.sh
+++ whoisentered.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+set -euo pipefail
 times=120 #in seconds
 CAM_DIR="$HOME/cam/.photos"
 CAM_DIR_SERV="Photo"
 userserv="YOUR_USER"
 ipserver="YOUR_IP_SERVER"
 portserv="YOUR_PORT_SERVER_SSH"
-mkdir -p $CAM_DIR
-cd $CAM_DIR
-if [[ -z $(dpkg -l | grep fswebcam) ]]; then
-		echo "Don't exist of fswebcam for correctly work.\nInstall.."
-		sudo apt install fswebcam || exit 1
+mkdir -p "$CAM_DIR"
+cd "$CAM_DIR" || exit 1
+if [[ -z $(command -v fswebcam) ]]; then
+	printf "Don't exist of fswebcam for correctly work.\nInstall.." && sudo apt install fswebcam -y || exit 1
 fi
 function doScreen(){
 data=$(date '+%Y-%m-%d_%H:%M:%S')
@@ -17,7 +17,7 @@ namescreenshot="screenshotWEBCAM.jpg"
 fswebcam -r 1280x720 --jpeg 85 -D 1 "$data$namescreenshot"
 sleep 1
 echo "Sending to Server.."
-scp -P $portserv "$CAM_DIR/$data$namescreenshot" $userserv@$ipserver:/home/$userserv/$CAM_DIR_SERV/ || echo -e "Server can't got save photo.\n\tPlease check to connect with your server."
+scp -P $portserv "$CAM_DIR/$data$namescreenshot" $userserv@$ipserver:/home/$userserv/$CAM_DIR_SERV/ || printf "Server can't got save photo.\n\tPlease check to connect with your server.\nOr try to do this:\n\t ssh-copy-id -p $portserv $userserv@$ipserver" && exit 1
 echo "Operations Send is ended."
 }
 
@@ -25,7 +25,7 @@ function doDieScreen(){
 data=$(date '+%Y-%m-%d_%H:%M:%S')
 namescreenshot="screenshotWEBCAMD.jpg"
 fswebcam -r 1280x720 --jpeg 85 -D 1 "$data$namescreenshot" #D is death
-scp -P $portserv "$CAM_DIR/$data$namescreenshot" $userserv@$ipserver:/home/$userserv/$CAM_DIR_SERV/ || echo -e "Server can't got save photo.\n\tPlease check to connect with your server."
+scp -P $portserv "$CAM_DIR/$data$namescreenshot" $userserv@$ipserver:/home/$userserv/$CAM_DIR_SERV/ || printf "Server can't got save photo.\n\tPlease check to connect with your server.\nOr try to do this:\n\t ssh-copy-id -p $portserv $userserv@$ipserver" && exit 1
 echo "Operations send is ended"
 exit 1
 }
